9a02c03e9d0a8962a29ecc933aaf900999c04f51
Theophile Nzungize <theophile.nz@gmail.com>
2017-02-21 01:36:40 -0500

    Official modifications

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
-var app = angular.module('portfolioApp', ["ngRoute"])
+var app = angular.module('portfolioApp', ["ngRoute", 'angular-typed'])
 
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "./vues/accueil.html",
             controller: "ControlleurPageAccueil"
-        })
-        .when("/outils", {
-            templateUrl: "./vues/outils.html",
-            controller: "ControlleurPageOutils"
-        })
-        .when("/projets", {
-            templateUrl: "./vues/projets.html",
-            controller: "ControlleurPageProjets"
-        })
-        .when("/demande-cv", {
-            templateUrl: "./vues/demande-cv.html",
-            controller: "ControlleurPageCV"
         })
         .otherwise({
             redirectTo: "./"
@@ -25,18 +13,24 @@ app.config(function($routeProvider) {
 });
 
 app.controller("ControlleurPageAccueil", ["$scope", function($scope) {
-    $scope.titre = "Je suis Théophile Nzungize";
-    $scope.sousTitre = "Je fais du développement, du design et du marketing web.";
-}]);
-
-app.controller("ControlleurPageOutils", ["$scope", function($scope) {
-    $scope.titre = "Mes outils de travail";
-}]);
-
-app.controller("ControlleurPageProjets", ["$scope", function($scope) {
-    $scope.titre = "Mes projets";
-}]);
-
-app.controller("ControlleurPageCV", ["$scope", function($scope) {
-    $scope.titre = "Demande de C.V";
+    $scope.competences = [{
+            nom: "HTML/CSS"
+        },
+        {
+            nom: "Javascript"
+        },
+        {
+            nom: "AngularJS"
+        },
+        {
+            nom: "jQuery"
+        }
+    ]
+    $scope.projets = [{
+            titre: "Zuo Design"
+        },
+        {
+            titre: "Meddexx"
+        }
+    ]
 }]);
